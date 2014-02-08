1ecc0deafa7975816806b7159d339d8b06f7852c
Jim Crossley <jim@crossleys.org>
2014-02-08 18:12:42 -0500

    Metadata

--- project.clj
+++ project.clj
@@ -1,6 +1,8 @@
 (defproject lein-modules "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "Similar to Maven multi-module projects, but less sucky"
+  :url "https://github.com/jcrossley3/lein-modules"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :eval-in-leiningen true)
+  :eval-in-leiningen true
+  :signing {:gpg-key "92439EF5"}
+  :lein-release {:deploy-via :clojars})
