527f7919f64f9ebf8558db336d574d4eb77975c1
Jim Crossley <jim@crossleys.org>
2014-02-16 11:48:04 -0500

    Spruce up a bit

--- src/lein_modules/compression.clj
+++ src/lein_modules/compression.clj
@@ -1,26 +1,35 @@
 (ns lein-modules.compression
+  "Only necessary because 2.3.4+ expands active profiles, and while
+  the child may include the composites, its expansions will be
+  different. This isn't foolproof."
   (:require [leiningen.core.project :as prj]))
 
-(defn expansions
-  [profiles]
-  (->> profiles
-    (filter (fn [[_ v]] (prj/composite-profile? v)))
-    (map (fn [[k v]]
-           (loop [[h & t] v, r []]
-             (cond
-               (nil? h) [r k]
-               (prj/composite-profile? (h profiles)) (recur (concat (h profiles) t), r)
-               :else (recur t, (conj r h))))))
-    (sort-by (comp count first))
-    reverse))
+(def ^:private expansions
+  "Returns a sequence of pairs where the first is the expansion of the
+  second, which is a key in profiles, ordered by largest expansion"
+  (memoize
+    (fn [profiles]
+      (->> profiles
+        (filter (fn [[_ v]] (prj/composite-profile? v)))
+        (map (fn [[k v]]
+               (loop [[h & t] v, r []]
+                 (cond
+                   (nil? h) [r k]
+                   (prj/composite-profile? (h profiles)) (recur (concat (h profiles) t), r)
+                   :else (recur t, (conj r h))))))
+        (sort-by (comp - count first))))))
 
-(defn replace-subvec
+(defn- replace-subvec
+  "Replace a sub-sequence within a vector with something else"
   [vect sub with]
   (vec (loop [v vect, result []]
          (cond
-           (empty? v) result
-           (< (count v) (count sub)) (concat result v)
-           (= sub (subvec v 0 (count sub))) (recur (subvec v (count sub)) (conj result with))
+           (< (count v) (count sub))
+           (concat result v)
+
+           (= sub (subvec v 0 (count sub)))
+           (recur (subvec v (count sub)) (conj result with))
+
            :else (recur (subvec v 1) (conj result (first v)))))))
 
 (defn compress
--- test/lein_modules/compression_test.clj
+++ test/lein_modules/compression_test.clj
@@ -7,4 +7,6 @@
     (is (= [:a] (compress [:b :d] profiles)))
     (is (= [:a :k] (compress [:b :d :k] profiles)))
     (is (= [:a :a :k] (compress [:a :b :d :k] profiles)))
-    (is (= [:a :c] (compress [:b :d :d] profiles)))))
+    (is (= [:a :c] (compress [:b :d :d] profiles)))
+    (is (= [:k :s :t] (compress [:k :s :t] profiles)))
+    (is (= [:k :s :t] (compress [:k :s :t] {})))))
