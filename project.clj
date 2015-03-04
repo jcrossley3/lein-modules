638cb57cabe50d31208fc77e21e5c2847d320f07
Jim Crossley <jim@crossleys.org>
2015-03-04 17:39:50 -0500

    Version 0.3.11

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject lein-modules "0.3.11-SNAPSHOT"
+(defproject lein-modules "0.3.11"
   :description "Similar to Maven multi-module projects, but less sucky"
   :url "https://github.com/jcrossley3/lein-modules"
   :license {:name "Eclipse Public License"
